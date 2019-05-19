46dccfa3c75aeab9f47e4a5a9439f9dcfd12bd0b
Jacob Hinze <jacob.hinze@hotmail.com>
2019-05-19 19:10:27 +0200

    Upgraded searchAPI to .Net Core 3.0

--- Server/Controllers/SearchController.cs
+++ Server/Controllers/SearchController.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Common.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Server.Services;
-using Lucene.Net.Search;
-using Common.Models;
 
-namespace SearchAPI.Controllers
+namespace Server.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
--- Server/Services/SearchService.cs
+++ Server/Services/SearchService.cs
@@ -1,7 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using Common.Models;
+using System;
+using System.Collections.Generic;
 using System.Linq;
-using Common.Models;
-using Lucene.Net.Search;
+using System.Threading.Tasks;
 
 namespace Server.Services
 {
@@ -16,7 +17,7 @@ namespace Server.Services
 
         public SearchResults GetResults(string term)
         {
-            return index.Search(term);    
+            return index.Search(term);
         }
     }
 }
